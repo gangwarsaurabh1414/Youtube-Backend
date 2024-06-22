d414753c20dbd296ec28260ffe27d6d1f3690b2f
Saurabh Gangwar <gangwarsaurabh1414@gmail.com>
2024-06-22 20:59:11 +0530

    cconnect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+import { configDotenv } from "dotenv";
+configDotenv();
+
+import connectDB from "./db/index.js";
+connectDB();
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.log("error : ", error);
+            throw error;
+        })
+        app.listen(`${process.env.PORT}`, () => {
+            console.log("App Is Listening to Port ", process.env.PORT);
+            console.log("Successfully Connected to DB");
+        })
+
+    } catch (error) {
+        console.error("Error : ", error);
+        throw error
+    }
+})()
+
+*/
